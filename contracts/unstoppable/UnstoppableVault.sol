52d4fb9463ec2dc734ba771e5f40c66dc8bd88ef
udeshika <perera.n.u@gmail.com>
2023-10-25 20:50:31 +0800

    updated audit comments

--- contracts/unstoppable/UnstoppableVault.sol
+++ contracts/unstoppable/UnstoppableVault.sol
@@ -90,6 +90,7 @@ contract UnstoppableVault is IERC3156FlashLender, ReentrancyGuard, Owned, ERC462
         uint256 amount,
         bytes calldata data
     ) external returns (bool) {
+        // @audit-info validation for amount and token
         if (amount == 0) revert InvalidAmount(0); // fail early
         if (address(asset) != _token) revert UnsupportedCurrency(); // enforce ERC3156 requirement
         uint256 balanceBefore = totalAssets();
